a6435b070f4714ddeae562c8c59af0766ed2a26f
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-05-02 19:02:21 +0100

    etudiant serializable

--- src/main/java/com/msid/gestionnotesetudiant/entities/Etudiant.java
+++ src/main/java/com/msid/gestionnotesetudiant/entities/Etudiant.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Etudiant implements Serializable {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName;
