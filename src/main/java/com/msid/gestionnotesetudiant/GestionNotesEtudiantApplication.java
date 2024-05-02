e9672f43b945490c62c5bf763c0c552cd9e8ec5f
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-05-02 17:22:05 +0100

    first push

---
+++ src/main/java/com/msid/gestionnotesetudiant/GestionNotesEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.msid.gestionnotesetudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotesEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotesEtudiantApplication.class, args);
+	}
+
+}
