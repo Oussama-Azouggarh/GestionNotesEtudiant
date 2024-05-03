3c3f562ac1051b678efaf1465f25dbf7fbedb12d
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-05-03 22:45:52 +0100

    att in Note class

--- src/main/java/com/msid/gestionnotesetudiant/entities/Note.java
+++ src/main/java/com/msid/gestionnotesetudiant/entities/Note.java
@@ -13,6 +13,7 @@ public class Note implements Serializable {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_Note;
     private double Valeur;
+    private int ayoub;
 
 
     @ManyToOne
